9e9e16045c616a0cca59ed3b6192839b03188d74
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2020-09-21 12:16:14 +0300

    add

--- src/components/profile/appointments/ProfileAppointments.js
+++ src/components/profile/appointments/ProfileAppointments.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ProfileCalendar from './ProfileAppointmentsCalendar';
-import ProfileAppointsmentsCards from './ProfileAppointmentsCards';
+import ProfileAppointmentsCards from './ProfileAppointmentsCards';
 import './ProfileAppointments.css';
 
 function ProfileAppointments() {
   return (
     <div className="allAppointments">
-      <ProfileAppointsmentsCards />
+      <ProfileAppointmentsCards />
       <ProfileCalendar />
     </div>
   );
